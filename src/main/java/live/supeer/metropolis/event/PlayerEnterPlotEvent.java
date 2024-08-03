de6bb2b455a8034c3951c7c4d9d838fa1b272401
Super <55873818+Sup33r@users.noreply.github.com>
2024-08-03 22:34:20 +0200

    Lade till mer i nya Eventsen

---
+++ src/main/java/live/supeer/metropolis/event/PlayerEnterPlotEvent.java
@@ -0,0 +1,28 @@
+package live.supeer.metropolis.event;
+
+import live.supeer.metropolis.plot.Plot;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class PlayerEnterPlotEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    private final Player player;
+    private final Plot plot;
+
+    public PlayerEnterPlotEvent(Player player, Plot plot) {
+        this.player = player;
+        this.plot = plot;
+    }
+
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
